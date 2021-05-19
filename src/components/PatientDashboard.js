f5d045930303ece0a71f413b6435e108dfcb94e9
Abhishek gowda M V <abhishek.mvg@gmail.com>
2021-05-19 14:44:10 +0530

    Common navbar for doctor and patient

--- src/components/PatientDashboard.js
+++ src/components/PatientDashboard.js
@@ -1,29 +1,15 @@
 import { React, useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import api from "./../api";
 import moment from "moment";
+import UserNavbar from "./UserNavbar";
 import "./../styles/patient-dashboard.css";
 
 export default function PatientDashboard({ match, history }) {
   const [consultationInfo, setConsultationInfo] = useState([]);
   const [patientInfo, setPatientInfo] = useState({ name: "", id: "" });
   const pid = match.params.id;
-  useEffect(() => {
-    //get all consultation details of the patient
-    //along with doctor name
-    api.get(`/consultation/patient/${pid}`).then((res) => {
-      res.data.map((con) => {
-        return api.get(`/doctor/${con.did}`).then((doc) => {
-          con["dname"] = doc.data[0].dname;
-          con["cdatetime"] = moment(con.cdatetime).format("h:mm a, MMM Do YY");
-          setConsultationInfo((prev) => [...prev, con]);
-        });
-      });
-    });
-  }, [pid]);
 
   useEffect(() => {
-    //get patient name and ID to display on navbar
     let parsed = Number.parseInt(pid);
     if (Number.isNaN(parsed)) {
       history.push("/404");
@@ -34,41 +20,39 @@ export default function PatientDashboard({ match, history }) {
         setPatientInfo({ name: res.data[0].pname, id: res.data[0].pid });
       }
     });
+
+    api.get(`/consultation/patient/${pid}`).then((res) => {
+      res.data.map((con) => {
+        return api.get(`/doctor/${con.did}`).then((doc) => {
+          con["dname"] = doc.data[0].dname;
+          con["cdatetime"] = moment(con.cdatetime).format(" h:mm a, MMM Do YY");
+          setConsultationInfo((prev) => [...prev, con]);
+        });
+      });
+    });
   }, [pid, history]);
 
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/patient/dashboard">Dashboard</Link>
-            </li>
-            <li className="nav-item">doctors</li>
-            <li className="nav-item">appointments</li>
-            <li className="nav-item">book an appointment</li>
-            <li className="nav-item info">
-              <strong>
-                {patientInfo.name}-{patientInfo.id}
-              </strong>
-            </li>
-          </ul>
+      <UserNavbar userRole={"patient"} userInfo={patientInfo} />
+      {consultationInfo.length === 0 ? (
+        <div style={{ textAlign: "center" }}>
+          <strong>All your consultation details will appear here</strong>
         </div>
-      </nav>
-      <div className="consultation-list">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Consultation No</th>
-              <th>Doctor name</th>
-              <th>Date & time</th>
-              <th>PDF</th>
-              <th>Audio file</th>
-            </tr>
-          </thead>
-          <tbody>
-            {consultationInfo && consultationInfo.length ? (
-              consultationInfo.map((consultation) => {
+      ) : (
+        <div className="consultation-list">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Consultation No</th>
+                <th>Doctor name</th>
+                <th>Date & time</th>
+                <th>PDF</th>
+                <th>Audio file</th>
+              </tr>
+            </thead>
+            <tbody>
+              {consultationInfo.map((consultation) => {
                 return (
                   <tr key={consultation.cid}>
                     <td>{consultation.cid}</td>
@@ -90,13 +74,11 @@ export default function PatientDashboard({ match, history }) {
                     </td>
                   </tr>
                 );
-              })
-            ) : (
-              <tr></tr>
-            )}
-          </tbody>
-        </table>
-      </div>
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/UserNavbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
+export default function UserNavbar({ userInfo, userRole }) {
+  const { name, id } = userInfo;
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to={`/${userRole}/${id}`}>Dashboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link to={`/${userRole}/${id}/appointment`}>Appointments</Link>
+            </li>
+            {userRole === "patient" ? (
+              <>
+                <li className="nav-item">
+                  <Link to="/doctors">Doctors</Link>
+                </li>
+                <li className="nav-item">Book an appointment</li>
+              </>
+            ) : (
+              ""
+            )}
+          </ul>
+        </div>
+        <span className="user-info">
+          <li className="nav-item">
+            <FontAwesomeIcon className="fa-icon" icon={faUser} />
+            {name}-{id}
+          </li>
+        </span>
+      </nav>
+    </div>
+  );
+}
