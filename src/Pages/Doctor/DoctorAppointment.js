a53299e801665d1b2f6a77e36677df2bab182124
Abhishek gowda M V <abhishek.mvg@gmail.com>
2021-06-28 00:33:23 +0530

    connected appointment to generate prescription page

--- src/Pages/Doctor/DoctorAppointment.js
+++ src/Pages/Doctor/DoctorAppointment.js
@@ -1,17 +1,104 @@
 import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router";
+import { Button } from "react-bootstrap";
+import UserNavbar from "./../../components/UserNavbar";
+import moment from "moment";
 import api from "../../api";
 
 export default function DoctorAppointment(props) {
   const did = props.match.params.id;
-  const [patientList, setPatientList] = useState([]);
+  const [appointmentList, setAppointmentList] = useState([]);
+  const [doctorInfo, setDoctorInfo] = useState([]);
+  const [path, setPath] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
   useEffect(() => {
+    api.get(`/doctor/${did}`).then((res) => {
+      if (res.data.length > 0) {
+        setDoctorInfo({ name: res.data[0].dname, id: res.data[0].did });
+      }
+    });
     api.get(`/appointment/doctor/${did}`).then(({ data }) => {
-      data.map((patientInfo) => {
-        return api.get(`/patient/${patientInfo.pid}`).then((data) => {
-          setPatientList((prev) => [...prev, data.data]);
-        });
+      data.map((appointmentInfo) => {
+        return api
+          .get(`/patient/${appointmentInfo.pid}`)
+          .then((patientData) => {
+            appointmentInfo["pname"] = patientData.data[0].pname;
+            appointmentInfo["pid"] = patientData.data[0].pid;
+            appointmentInfo["booking_date"] = moment(
+              appointmentInfo.booking_date
+            ).format("MMM Do");
+            appointmentInfo["start_time"] = moment(
+              appointmentInfo.start_time,
+              "hh:mm"
+            ).format("hh:mm a");
+
+            setAppointmentList((prev) => [...prev, data]);
+          });
       });
     });
   }, [did]);
-  return <div>{console.log(patientList)}</div>;
+
+  if (redirect) {
+    return (
+      <Redirect
+        to={{
+          pathname: path,
+        }}
+      />
+    );
+  }
+
+  return (
+    <div>
+      <UserNavbar userRole="doctor" userInfo={doctorInfo} />
+      {appointmentList.length === 0 ? (
+        <div style={{ textAlign: "center", marginTop: "100px" }}>
+          <strong>All your consultation details will appear here</strong>
+        </div>
+      ) : (
+        <div className="consultation-list">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Appointment ID</th>
+                <th>Patient ID</th>
+                <th>Patient name</th>
+                <th>Slot</th>
+                <th>Date & time</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {appointmentList[0].map((appointment, idx) => {
+                return (
+                  <tr key={idx}>
+                    <td>{appointment.bid}</td>
+                    <td>{appointment.pid}</td>
+                    <td>{appointment.pname}</td>
+                    <td>{appointment.slot_no}</td>
+                    <td>
+                      {appointment.booking_date}, {appointment.start_time}
+                    </td>
+                    <td>
+                      <Button
+                        onClick={() => {
+                          setPath(
+                            `/doctor/${did}/process-prescription/${appointment.pid}`
+                          );
+                          setRedirect(true);
+                        }}
+                      >
+                        Make prescription
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 }
