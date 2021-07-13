b21ee49993bbd05be5db8a597f04d8ca1fa46ef2
Abhishek gowda M V <abhishek.mvg@gmail.com>
2021-07-13 13:30:08 +0530

    a tag to link

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,40 +1,53 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { Card} from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import "./../styles/homepage.css";
+import { Link } from "react-router-dom";
 import doc from "./../images/docc.jpeg";
 import pat from "./../images/patt.jpeg";
 
 export default function Homepage() {
   return (
-    <><div className="backk">
-    <center>
-    
-    <div className="container">
-    <div className="flexx">
-    <Card style={{ width: "30rem", height:"30rem" }}>
-      <Card.Body>
-        <img src={pat} width="400px" height="400px"></img>
-        <Card.Text><button type="button" className="rand"  ><a href="/patient/login">Patient Login </a>
-        
-        </button>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    <Card style={{ width: "30rem",height:"30rem", marginLeft:"50px" }}>
-      <Card.Body>
-        <img src={doc} width="400px" height="400px"></img>
-        <Card.Text><button type="button" className="rand" ><a href="/doctor/login">Doctor Login  </a>
-        </button>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-    </div>
-      
-      
-    </div>
-    </center> </div>
+    <>
+      <div className="backk">
+        <center>
+          <div className="container">
+            <div className="flexx">
+              <Card style={{ width: "30rem", height: "30rem" }}>
+                <Card.Body>
+                  <img
+                    src={pat}
+                    width="400px"
+                    alt="patient"
+                    height="400px"
+                  ></img>
+                  <Card.Text>
+                    <button type="button" className="rand">
+                      <Link to="/patient/login">Patient Login </Link>
+                    </button>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card
+                style={{ width: "30rem", height: "30rem", marginLeft: "50px" }}
+              >
+                <Card.Body>
+                  <img
+                    src={doc}
+                    width="400px"
+                    alt="patient"
+                    height="400px"
+                  ></img>
+                  <Card.Text>
+                    <button type="button" className="rand">
+                      <Link to="/doctor/login">Doctor Login </Link>
+                    </button>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </center>{" "}
+      </div>
     </>
   );
 }
